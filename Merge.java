03d83b2bb40ca673953091407cc84d5a9b99c149
Amanda Chen <achen01@stuy.edu>
2019-03-17 12:34:49 -0400

    finished writing merge sort

--- Merge.java
+++ Merge.java
@@ -2,13 +2,28 @@ public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
     int[] temp = new int[data.length];
-    mergeH(data,0,data.length-1)
+    mergeH(data,0,data.length-1);
   }
 
-  private static void mergeH(int[]data, int lo, int hi){
-    if (lo >= hi){
+  private static void mergeH(int[] data, int lo, int hi){
+    if (lo >= hi+1){
       return;
     }
-    mergesort(data,0,hi/2);
+    int[] temp = new int[data.length];
+    int j = 0;
+    mergeH(data,lo,(lo+hi)/2);
+    mergeH(data,(lo+hi)/2+1,hi);
+    int a = lo;
+    int b = (lo+hi)/2+1;
+    while (a < (lo+hi)/2+1 && b < hi+1){
+      if (a <= b){
+        temp[j] = data[a];
+        a++;
+      } else {
+        temp[j] = data[b];
+        b++;
+      }
+      j++;
+    }
   }
 }
