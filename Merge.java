006c190d562e87d55cea74219b039b1f54bbfa44
Amanda Chen <achen01@stuy.edu>
2019-03-17 16:04:59 -0400

    fixed stack overflow

--- Merge.java
+++ Merge.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
@@ -6,7 +8,7 @@ public class Merge{
   }
 
   private static void mergeH(int[] data, int lo, int hi){
-    if (lo >= hi+1){
+    if (lo >= hi){
       return;
     }
     int[] temp = new int[data.length];
@@ -26,4 +28,39 @@ public class Merge{
       j++;
     }
   }
+
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Merge.mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
 }
